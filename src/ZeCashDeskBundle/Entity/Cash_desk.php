a4956cb532e00b86ca0cb1e8c90294ec084fd7e7
Miguel <themig@laposte.net>
2017-03-13 14:06:18 +0100

    Fix_CRUD=OK

--- src/ZeCashDeskBundle/Entity/Cash_desk.php
+++ src/ZeCashDeskBundle/Entity/Cash_desk.php
@@ -13,7 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
 class Cash_desk
 {
     /**
-     * @ORM\OneToOne(targetEntity="ZeCashDeskBundle\Entity\Users", inversedBy="Cash_desk")
+     * @ORM\OneToOne(targetEntity="ZeCashDeskBundle\Entity\Users")
      */
     private $users;
 
