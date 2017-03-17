970e1825c9e4886619e805d148fbbfe0e2737e0d
David <david.cossedu@gmail.com>
2017-03-17 16:55:32 +0100

    bouine pour réussir a utiliser notre gencode

--- web/js/my-js.js
+++ web/js/my-js.js
@@ -1,6 +1,6 @@
 $(init);
 
-function init () {
+function init() {
     afficher();
     setInterval(afficher, 1000);
     codebarre();
@@ -9,12 +9,20 @@ function init () {
 function afficher() {
     var jours = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
     lD = new Date();
-    document.getElementById('heure').innerHTML = '' + jours[lD.getDay()] + '  ' + lD.toLocaleString()+'';
+    document.getElementById('heure').innerHTML = '' + jours[lD.getDay()] + '  ' + lD.toLocaleString() + '';
 
 }
 
 
-function codebarre(){
+function codebarre() {
 
+    $.ajax({
+        url: '/terminal/genCode/{gencode}',
+        method: 'GET',
+    }).done(function (data) {
+
+
+    });
+    $('#ticket').append($_GET['codebarre']);
 
 }
